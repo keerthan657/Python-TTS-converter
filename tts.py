7c59d4d401c06079776c894a5a9d70a96daf6e24
keerthan657 <keerthankk212001@gmail.com>
2020-05-22 12:05:02 +0530

    New updated script
    Implemented new features like volume, rate and tone of voice

--- tts.py
+++ tts.py
@@ -1,3 +1,5 @@
+#Created by ~ Mr.KK
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 import pyttsx3
@@ -7,8 +9,8 @@ import time
 #This is the pyttsx3 speech engine
 engine = pyttsx3.init()
 
-#To check if the delay time is valid or not
-def is_integer(n):
+#To check if the delay time is valid or not (i.e, should be a positive number from 0 to 10 sec)
+def is_valid(n):
 	try:
 		if(int(n)>=0 and int(n)<=10):
 			return True
@@ -17,12 +19,10 @@ def is_integer(n):
 	except ValueError:
 		return False
 
-
-#Implementing the application with PyQt5
 class Ui_PythonSpeaker(object):
 	def setupUi(self, PythonSpeaker):
 		PythonSpeaker.setObjectName("PythonSpeaker")
-		PythonSpeaker.resize(789, 202)
+		PythonSpeaker.resize(758, 244)
 		self.centralwidget = QtWidgets.QWidget(PythonSpeaker)
 		self.centralwidget.setObjectName("centralwidget")
 		self.pushButton = QtWidgets.QPushButton(self.centralwidget)
@@ -34,12 +34,32 @@ class Ui_PythonSpeaker(object):
 		self.textEdit1 = QtWidgets.QTextEdit(self.centralwidget)
 		self.textEdit1.setGeometry(QtCore.QRect(20, 50, 531, 121))
 		self.textEdit1.setObjectName("textEdit1")
-		self.textEdit2 = QtWidgets.QTextEdit(self.centralwidget)
-		self.textEdit2.setGeometry(QtCore.QRect(170, 10, 41, 31))
-		self.textEdit2.setObjectName("textEdit2")
+		self.delayEdit = QtWidgets.QTextEdit(self.centralwidget)
+		self.delayEdit.setGeometry(QtCore.QRect(170, 10, 41, 31))
+		self.delayEdit.setObjectName("delayEdit")
 		self.label = QtWidgets.QLabel(self.centralwidget)
 		self.label.setGeometry(QtCore.QRect(70, 10, 91, 31))
 		self.label.setObjectName("label")
+		self.volumeSlider = QtWidgets.QSlider(self.centralwidget)
+		self.volumeSlider.setGeometry(QtCore.QRect(110, 190, 160, 22))
+		self.volumeSlider.setOrientation(QtCore.Qt.Horizontal)
+		self.volumeSlider.setObjectName("volumeSlider")
+		self.label_2 = QtWidgets.QLabel(self.centralwidget)
+		self.label_2.setGeometry(QtCore.QRect(40, 190, 55, 16))
+		self.label_2.setObjectName("label_2")
+		self.label_3 = QtWidgets.QLabel(self.centralwidget)
+		self.label_3.setGeometry(QtCore.QRect(320, 20, 61, 16))
+		self.label_3.setObjectName("label_3")
+		self.rateSlider = QtWidgets.QSlider(self.centralwidget)
+		self.rateSlider.setGeometry(QtCore.QRect(400, 20, 160, 22))
+		self.rateSlider.setOrientation(QtCore.Qt.Horizontal)
+		self.rateSlider.setObjectName("rateSlider")
+		self.maleVoiceCheckbox = QtWidgets.QCheckBox(self.centralwidget)
+		self.maleVoiceCheckbox.setGeometry(QtCore.QRect(410, 190, 101, 20))
+		self.maleVoiceCheckbox.setObjectName("maleVoiceCheckbox")
+		self.femaleVoiceCheckbox = QtWidgets.QCheckBox(self.centralwidget)
+		self.femaleVoiceCheckbox.setGeometry(QtCore.QRect(520, 190, 111, 20))
+		self.femaleVoiceCheckbox.setObjectName("femaleVoiceCheckbox")
 		PythonSpeaker.setCentralWidget(self.centralwidget)
 		self.statusbar = QtWidgets.QStatusBar(PythonSpeaker)
 		self.statusbar.setObjectName("statusbar")
@@ -55,29 +75,59 @@ class Ui_PythonSpeaker(object):
 		self.pushButton.setStatusTip(_translate("PythonSpeaker", "Click me to speak-out"))
 		self.pushButton.setText(_translate("PythonSpeaker", "Speak"))
 		self.textEdit1.setStatusTip(_translate("PythonSpeaker", "Type your words here !"))
-		self.textEdit2.setStatusTip(_translate("PythonSpeaker", "Delay time - To delay the speaking from 0 to 10 sec"))
+		self.delayEdit.setStatusTip(_translate("PythonSpeaker", "Delay time - To delay the speaking (in sec)"))
 		self.label.setText(_translate("PythonSpeaker", "Set delay time :"))
+		self.volumeSlider.setStatusTip(_translate("PythonSpeaker", "Slide me to chage the volume"))
+		self.label_2.setText(_translate("PythonSpeaker", "Volume :"))
+		self.label_3.setText(_translate("PythonSpeaker", "Set Rate :"))
+		self.rateSlider.setStatusTip(_translate("PythonSpeaker", "Set your voice rate here"))
+		self.maleVoiceCheckbox.setText(_translate("PythonSpeaker", "Male Voice"))
+		self.femaleVoiceCheckbox.setText(_translate("PythonSpeaker", "Female Voice"))
 
 		self.pushButton.clicked.connect(self.on_click)
+		self.delayEdit.setText('0')             #Default delay time
+		self.rateSlider.setMinimum(1)
+		self.rateSlider.setMaximum(250)
+		self.rateSlider.setValue(175)           #This is the default state of rate slider
+		self.volumeSlider.setValue(100)         #This is the default state of volume slider
+		self.maleVoiceCheckbox.setChecked(True)
 
-	#This error dialog is shown if delay time is a invalid entry
-	def error(self):
+	#This error is shown if the text-field is empty
+	def errorEmptyText(self, message):
 		errorDialog = QMessageBox()
 		errorDialog.setIcon(QMessageBox.Critical)
-		errorDialog.setText("Error in delay time. Make sure delay is between 0 and 10 sec")
-		errorDialog.setWindowTitle("Error reading")
+		errorDialog.setText(message)
+		errorDialog.setWindowTitle("Reading error")
 		errorDialog.exec_()
 
 	def on_click(self):
 		myText = self.textEdit1.toPlainText()
-		delayTime = self.textEdit2.toPlainText()
-		if(is_integer(delayTime) == False):
-			self.error()
+		delayTime = self.delayEdit.toPlainText()
+		rateValue = self.rateSlider.value()
+		volumeValue = self.volumeSlider.value() / 100.0
+
+		if(is_valid(delayTime) == False):
+			self.errorEmptyText("Error reading delay time. Make sure delay time is an integer between 0 and 10 sec")
+			return
+		if(myText == ''):
+			self.errorEmptyText("Please enter text to be spoken!!")
 		else:
+			engine.setProperty('rate', rateValue)        #default voice rate would be 200
+			engine.setProperty('volume' , volumeValue)   #setting up volume level  between 0 and 1
+			if(self.maleVoiceCheckbox.isChecked() == True and self.femaleVoiceCheckbox.isChecked() == False):
+				VoiceId = 0
+			elif(self.femaleVoiceCheckbox.isChecked() == True and self.maleVoiceCheckbox.isChecked() == False):
+				VoiceId = 1
+			else:
+				self.maleVoiceCheckbox.setChecked(True)
+				self.femaleVoiceCheckbox.setChecked(False)
+				VoiceId = 0                             #Defaluts to male voice if both are selected or if none are selected
+
+			voices = engine.getProperty('voices')
+			engine.setProperty('voice', voices[VoiceId].id)     #VoiceId = 0 for male voice and 1 for female voice
 			time.sleep(int(delayTime))
 			engine.say(myText)
 			engine.runAndWait()
-
 
 if __name__ == "__main__":
 	import sys
